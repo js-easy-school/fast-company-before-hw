fb8ee22ee1ae3f9aa2c15c5477380d100a10bcb7
js-easy-school <supergeroy11@gmail.com>
2023-08-29 16:24:21 +0300

    modified:   src/app/components/bookmark.jsx

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 const BookMark = ({ status, ...rest }) => {
+    console.log(status);
     return (
         <button {...rest}>
             <i className={"bi bi-bookmark" + (status ? "-heart-fill" : "")}></i>
