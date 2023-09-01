47b04b84e96f3babb361d64cbf57015126ae7903
Андрей Мазунин (Andrey Mazunin) <97140182+js-easy-school@users.noreply.github.com>
2023-09-01 16:12:24 +0300

    Update and rename .eslintrc to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ["standard", "plugin:react/recommended"],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script"
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["react"],
+    rules: {
+        semi: ["error", "always"],
+        // "indent": ["error", 4],
+        "space-before-function-paren": "off",
+        quotes: "off",
+        curly: "off",
+        // "curly": ["error", "multi-line"],
+        indent: ["error", 4, { SwitchCase: 1 }],
+        "default-case": "error",
+        "no-fallthrough": "error",
+        "comma-dangle": "off"
+    }
+};
